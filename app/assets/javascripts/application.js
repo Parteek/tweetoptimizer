7ef9a9af29f0e3926f118185ad9b2b65ae058ae6
Parteek Singla <parteekiitp@gmail.com>
2016-02-08 21:04:33 +0530

    implementation completed with graphs and all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,124 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+function build_day_graph(days){
+    $('#days-container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Followers Tweet Frequency'
+        },
+        subtitle: {
+            text: 'Distributed over weekdays'
+        },
+        xAxis: {
+            categories: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Tweet Count'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Tweets',
+            data: days
+        }]
+    });
+}
+
+function build_time_graph(times){
+    $('#times-container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Followers Tweet Frequency'
+        },
+        subtitle: {
+            text: 'Distributed over hours in a day'
+        },
+        xAxis: {
+            categories: [
+                '0:00 - 1:00',
+                '1:00 - 2:00',
+                '2:00 - 3:00',
+                '3:00 - 4:00',
+                '4:00 - 5:00',
+                '5:00 - 6:00',
+                '6:00 - 7:00',
+                '7:00 - 8:00',
+                '8:00 - 9:00',
+                '9:00 - 10:00',
+                '10:00 - 11:00',
+                '11:00 - 12:00',
+                '12:00 - 13:00',
+                '13:00 - 14:00',
+                '14:00 - 15:00',
+                '15:00 - 16:00',
+                '16:00 - 17:00',
+                '17:00 - 18:00',
+                '18:00 - 19:00',
+                '19:00 - 20:00',
+                '20:00 - 21:00',
+                '21:00 - 22:00',
+                '22:00 - 23:00',
+                '23:00 - 24:00',
+            ],
+            crosshair: true
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Tweet Count'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Tweets',
+            data: times
+        }]
+    });
+}
+
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,78 @@
+class HomeController < ApplicationController
+  #before_action :set_twitter_params, only: [:best_day_time]
+
+  def index
+
+  end
+
+  def best_day_time
+    begin
+      response = TC.followers(twitter_params[:username])
+      @result = find_best_day_and_time(response)
+    rescue
+      @error = 'Could not fetch the results'
+    end
+  end
+
+  private
+
+  def twitter_params
+    params.require(:twitter).permit(:username)
+  end
+
+  #params data is the followers list
+  def find_best_day_and_time(data)
+    result = {}
+    if data.present?
+      best_day, best_time = Hash.new(0), Hash.new(0)
+
+      days = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+
+      day, time = initialize_date_hash(days), initialize_time_hash
+
+      data.each do |follower|
+        if follower.status.present?
+          # convert the string into date
+          last_tweet_date = DateTime.parse follower.status.created_at.to_s
+
+          # fetch the hour of the date, increment the count for that hour and set the best time
+          last_tweet_hour = "#{last_tweet_date.hour}:00 - #{last_tweet_date.hour+1}:00"
+          time[last_tweet_hour] += 1
+          if time[last_tweet_hour] > best_time[:count]
+            best_time[:count] = time[last_tweet_hour]
+            best_time[:value] = last_tweet_hour
+          end
+
+          # fetch the day of the date, increment the count for that day and set the best day
+          last_tweet_day = days[last_tweet_date.wday]
+          day[last_tweet_day] += 1
+          if day[last_tweet_day] > best_day[:count]
+            best_day[:count] = day[last_tweet_day]
+            best_day[:value] = last_tweet_day
+          end
+        end
+      end
+
+      result[:day] = day
+      result[:best_day] = best_day
+      result[:time] = time
+      result[:best_time] = best_time
+    end
+    result
+  end
+
+  # set 0 for all the days in the week.
+  def initialize_day_hash(days)
+    days.each do |d|
+      day[d] = 0
+    end
+  end
+
+  # set 0 for all the hour ranges in a day.
+  def initialize_time_hash
+    time = {}
+    (0..23).each do |hr|
+      time["#{hr}:00 - #{hr+1}:00"] = 0
+    end
+  end
+end
